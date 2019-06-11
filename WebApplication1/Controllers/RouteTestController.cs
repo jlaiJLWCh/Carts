9b8fa3685f64d975e979b4792f2259de034e5e1a
jlaiJLWCh <john910617850331@gmail.com>
2019-06-11 21:57:16 +0800

    Day4

---
+++ WebApplication1/Controllers/RouteTestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class RouteTestController : Controller
+    {
+        // GET: RouteTest
+        public ActionResult Index()
+        {
+            return Content("這是Index");
+        }
+
+        public ActionResult Index2(string id)
+        {
+            return Content(
+                String.Format("id的值為:{0}", id)
+                );
+        }
+
+        public ActionResult Index3(string id, string page)
+        {
+            return Content(
+                String.Format("id的值為:{0}, page的值為:{1}", id, page)
+                );
+        }
+    }
+}
